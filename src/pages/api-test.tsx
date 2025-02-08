e0c60c1d4d90b210cfb2dda7555471ff7c902183
Granth J <granth.jain@gmail.com>
2025-02-08 17:11:08 +0000

    added api-test

---
+++ src/pages/api-test.tsx
@@ -0,0 +1,25 @@
+import { Suspense, useEffect, useState } from "react";
+import Page from "./Page";
+
+function SampleAPIRequest() {
+    const [data, setData] = useState(null);
+  
+    useEffect(() => {
+      fetch("/api/sample_request")
+        .then((res) => res.json())
+        .then((data) => setData(data))
+        .catch((error) => console.error("Error fetching API:", error));
+    }, []);
+  
+    return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  }
+
+export default function SampleAPI() {
+    return (
+        <Page tabName="API Test">
+            <Suspense fallback={<div>Loading api data...</div>}>
+                <SampleAPIRequest/>
+            </Suspense>
+        </Page>
+    )
+}
