871db3daf8f473f02c3be92e54681c213cd9f1ae
Granth J <granth.jain@gmail.com>
2025-02-08 17:13:49 +0000

    Added some fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from "next/head";
-import Page from "./Page";
 
 export default function Root() {
     return (
-    <Page tabName="Empty tbd">
-        Empty page content tbd
-    </Page>
+    <>
+        <Head key="a">
+            <title>Login</title>
+        </Head>
+    </>
     )
 }
 
