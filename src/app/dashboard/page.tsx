1c528ce9ae686f93b2339cdffb4795e4b10ca20f
Granth J <granth.jain@gmail.com>
2025-02-11 17:17:12 +0000

    as

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -31,14 +31,12 @@ export default function Dashboard() {
     setIsFullscreen(!isFullscreen);
   };
 
-
-
   useEffect(() => {
     fetchProjects();
   }, []);
 
   return (
-    <Layout tabName={"Dashboard"} icon={<i className="fa-solid fa-table-columns"></i>}>
+    <Layout tabName={"Welcome"} icon={<i className="fa-solid fa-table-columns"></i>}>
       <div className={`h-full p-4 ps-0 ${isFullscreen ? 'hidden' : ''}`}>
         <div className="h-full">
           {/* Top Section */}
