213ea7133d6c3f939c4ba9df3528686774ee01f7
Squire11 <112522257+Squire11@users.noreply.github.com>
2025-02-15 10:56:26 +0000

    Update page.tsx

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,7 +1,29 @@
+"use client";
 import Layout from "../layout/page";
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { fetchTasks } from "@/api/fetchTasks";
+import { Task } from "@/interfaces/interfaces";
 export default function TasksPage() {
+  const [selectedTask, setSelectedTask] = useState<Task | null>(null);
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchTasks(0);
+        console.log("Fetched tasks:", data);
+        setTasks(data);
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleRowClick = (task: Task) => {
+    setSelectedTask(task);
+  };
+
   return (
     <Layout tabName="Tasks" icon={<i className="fa-solid fa-tasks"></i>}>
       {/* Grid Layout with 2/3 + 1/3 Column Split */}
@@ -31,14 +53,17 @@ export default function TasksPage() {
               <table className="w-full">
                 <thead className="bg-[#1f2937] text-gray-50 uppercase tracking-wider text-left text-xs">
                   <tr>
-                    <th className="table-cell px-3 py-3 text-center font-normal rounded-tl-lg">
-                      Select
+                    <th className="table-cell font-normal">
+                      <i className="fa-solid fa-font"></i> Task Id
                     </th>
                     <th className="table-cell font-normal">
-                      <i className="fa-solid fa-font"></i> Project Name
+                      <i className="fa-solid fa-font"></i> Task Name
                     </th>
                     <th className="table-cell font-normal">
-                      <i className="fa-solid fa-user"></i> Team Lead
+                      <i className="fa-solid fa-font"></i> Project Id
+                    </th>
+                    <th className="table-cell font-normal">
+                      <i className="fa-solid fa-calendar-days"></i> Start Date
                     </th>
                     <th className="table-cell font-normal">
                       <i className="fa-solid fa-calendar-days"></i> Due Date
@@ -46,144 +71,72 @@ export default function TasksPage() {
                     <th className="table-cell text-center font-normal">
                       Status
                     </th>
-                    <th className="table-cell text-center font-normal rounded-tr-lg">
-                      Notes
-                    </th>
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200 text-sm">
-                  <tr className="hover:bg-blue-50">
-                    <td className="table-cell px-2 py-3 text-center">
-                      <input type="checkbox" className="w-6 h-6" />
-                    </td>
-                    <td className="table-cell px-2 py-3">Project 1</td>
-                    <td className="table-cell px-2 py-3">Team Lead A</td>
-                    <td className="table-cell px-2 py-3 text-gray-500">
-                      2023-01-01
-                    </td>
-                    <td className="table-cell px-2 py-3 text-green-600 text-center">
-                      Completed
-                    </td>
-                    <td className="table-cell px-2 py-3 text-center"></td>
-                  </tr>
-                  <tr className="hover:bg-blue-50">
-                    <td className="table-cell px-2 py-3 text-center">
-                      <input type="checkbox" className="w-6 h-6" />
-                    </td>
-                    <td className="table-cell px-2 py-3">Project 1</td>
-                    <td className="table-cell px-2 py-3">Team Lead A</td>
-                    <td className="table-cell px-2 py-3 text-gray-500">
-                      2023-01-01
-                    </td>
-                    <td className="table-cell px-2 py-3 text-green-600 text-center">
-                      Completed
-                    </td>
-                    <td className="table-cell px-2 py-3 text-center"></td>
-                  </tr>
-                  <tr className="hover:bg-blue-50">
-                    <td className="table-cell px-2 py-3 text-center">
-                      <input type="checkbox" className="w-6 h-6" />
-                    </td>
-                    <td className="table-cell px-2 py-3">Project 1</td>
-                    <td className="table-cell px-2 py-3">Team Lead A</td>
-                    <td className="table-cell px-2 py-3 text-gray-500">
-                      2023-01-01
-                    </td>
-                    <td className="table-cell px-2 py-3 text-green-600 text-center">
-                      Completed
-                    </td>
-                    <td className="table-cell px-2 py-3 text-center"></td>
-                  </tr>
-                  <tr className="hover:bg-blue-50">
-                    <td className="table-cell px-2 py-3 text-center">
-                      <input type="checkbox" className="w-6 h-6" />
-                    </td>
-                    <td className="table-cell px-2 py-3">Project 1</td>
-                    <td className="table-cell px-2 py-3">Team Lead A</td>
-                    <td className="table-cell px-2 py-3 text-gray-500">
-                      2023-01-01
-                    </td>
-                    <td className="table-cell px-2 py-3 text-green-600 text-center">
-                      Completed
-                    </td>
-                    <td className="table-cell px-2 py-3 text-center"></td>
-                  </tr>
-                  <tr className="hover:bg-blue-50">
-                    <td className="table-cell px-2 py-3 text-center">
-                      <input type="checkbox" className="w-6 h-6" />
-                    </td>
-                    <td className="table-cell px-2 py-3">Project 1</td>
-                    <td className="table-cell px-2 py-3">Team Lead A</td>
-                    <td className="table-cell px-2 py-3 text-gray-500">
-                      2023-01-01
-                    </td>
-                    <td className="table-cell px-2 py-3 text-green-600 text-center">
-                      Completed
-                    </td>
-                    <td className="table-cell px-2 py-3 text-center"></td>
-                  </tr>
-                  <tr className="hover:bg-blue-50">
-                    <td className="table-cell px-2 py-3 text-center">
-                      <input type="checkbox" className="w-6 h-6" />
-                    </td>
-                    <td className="table-cell px-2 py-3">Project 1</td>
-                    <td className="table-cell px-2 py-3">Team Lead A</td>
-                    <td className="table-cell px-2 py-3 text-gray-500">
-                      2023-01-01
-                    </td>
-                    <td className="table-cell px-2 py-3 text-green-600 text-center">
-                      Completed
-                    </td>
-                    <td className="table-cell px-2 py-3 text-center"></td>
-                  </tr>
-                  <tr className="hover:bg-blue-50">
-                    <td className="table-cell px-2 py-3 text-center">
-                      <input type="checkbox" className="w-6 h-6" />
-                    </td>
-                    <td className="table-cell px-2 py-3">Project 1</td>
-                    <td className="table-cell px-2 py-3">Team Lead A</td>
-                    <td className="table-cell px-2 py-3 text-gray-500">
-                      2023-01-01
-                    </td>
-                    <td className="table-cell px-2 py-3 text-green-600 text-center">
-                      Completed
-                    </td>
-                    <td className="table-cell px-2 py-3 text-center"></td>
-                  </tr>
-                  <tr className="bg-gray-50 hover:bg-blue-50">
-                    <td className="table-cell px-2 py-3 text-center">
-                      <input type="checkbox" className="w-6 h-6" />
-                    </td>
-                    <td className="table-cell px-2 py-3">Project 2</td>
-                    <td className="table-cell px-2 py-3">Team Lead B</td>
-                    <td className="table-cell px-2 py-3 text-gray-500">
-                      2023-02-01
-                    </td>
-                    <td className="table-cell px-2 py-3 text-yellow-600 text-center">
-                      Unfinished
-                    </td>
-                    <td className="table-cell px-2 py-3 text-center"></td>
-                  </tr>
-                  <tr className="hover:bg-blue-50">
-                    <td className="table-cell px-2 py-3 text-center">
-                      <input type="checkbox" className="w-6 h-6" />
-                    </td>
-                    <td className="table-cell px-2 py-3">Project 3</td>
-                    <td className="table-cell px-2 py-3">Team Lead C</td>
-                    <td className="table-cell px-2 py-3 text-gray-500">
-                      2023-03-01
-                    </td>
-                    <td className="table-cell px-2 py-3 text-gray-500 text-center">
-                      Not Started
-                    </td>
-                    <td className="table-cell px-2 py-3 text-center"></td>
-                  </tr>
+                  {tasks.map((task, index) => (
+                    <tr
+                      key={index}
+                      className="bg-gray-50 hover:bg-blue-50 cursor-pointer"
+                      onClick={() => handleRowClick(task)}
+                    >
+                      <td className="table-cell px-2 py-3">{task.task_id}</td>
+                      <td className="table-cell px-2 py-3">{task.task_name}</td>
+                      <td className="table-cell px-2 py-3">
+                        {task.project_id}
+                      </td>
+                      <td className="table-cell px-2 py-3 text-gray-500">
+                        {task.start_date.toLocaleDateString()}
+                      </td>
+                      <td className="table-cell px-2 py-3 text-gray-500">
+                        {task.finish_date.toLocaleDateString()}
+                      </td>
+                      <td className="table-cell px-2 py-3 text-yellow-600 text-center">
+                        {task.completed}
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
+          </div>
+        </div>
+
+        {/* Right Column (1/3 width) */}
+        <div className="col-span-1 flex flex-col gap-6">
+          <div className="bg-white shadow-xl border rounded-xl p-5 h-[35em]">
+            <h2 className="text-xl font-bold mb-3">Task Details</h2>
+            {/* Display task details if a task is selected */}
+            {selectedTask ? (
+              <div className="flex flex-col gap-3">
+                <p>
+                  <strong>Task Name:</strong> {selectedTask.task_name}
+                </p>
+                <p>
+                  <strong>Project:</strong> {selectedTask.project_id}
+                </p>
+                <p>
+                  <strong>Start Date:</strong>{" "}
+                  {selectedTask.start_date.toLocaleDateString()}
+                </p>
+                <p>
+                  <strong>Due Date:</strong>{" "}
+                  {selectedTask.finish_date.toLocaleDateString()}
+                </p>
+                <p>
+                  <strong>Status:</strong> {selectedTask.completed}
+                </p>
+                <p>
+                  <strong>Description:</strong> {selectedTask.description}
+                </p>
+              </div>
+            ) : (
+              <p>Select a task to view details.</p>
+            )}
           </div>
         </div>
       </div>
     </Layout>
   );
 }
+
