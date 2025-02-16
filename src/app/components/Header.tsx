b15bf2588824ec0b2638fdd52c0dece4e2ef1d5a
Jake Brooks <jakebrooks342@gmail.com>
2025-02-16 14:48:21 +0000

    Sidebar and header change based on user role

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -17,7 +17,7 @@ const Header = ({ userRole, onRoleChange, tabName, icon = ""}: HeaderProps) => {
     useEffect(() => {
         const userData = JSON.parse(localStorage.getItem('userData') || '{}');
         console.log(JSON.stringify(userData));
-        if (userData && userData.user_type_id) {
+        if (userData && userData.user_type_id !== undefined) {
             // Map the user_type_id to a userRole string
             const roleMap: Record<number, UserRole> = {
                 0: "Manager",
