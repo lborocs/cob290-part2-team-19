5455ef3a273411c5e17c4154a68b7ddddee36275
Case <casempage@gmail.com>
2025-02-17 08:11:00 +0000

    local

--- src/api/globals.ts
+++ src/api/globals.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://35.240.24.117:3300";
-// export const BASE_URL = "localhost:3300";
+//export const BASE_URL = "http://35.240.24.117:3300";
+export const BASE_URL = "http://localhost:3300";
