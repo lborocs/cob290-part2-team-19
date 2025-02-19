ec052df4ff710da4ec985159a8412b6bd7f741b5
Jake Brooks <jakebrooks342@gmail.com>
2025-02-19 12:33:52 +0000

    fix

--- db.py
+++ db.py
@@ -1794,7 +1794,7 @@ def get_employee_details(employee_id):
     
         
 if __name__ == '__main__':
-    app.run(port=PORT)
+    app.run(host="0.0.0.0", port=3300)
     init_db()
     while True:
         schedule.run_pending()
